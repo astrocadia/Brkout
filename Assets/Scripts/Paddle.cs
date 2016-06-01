6771eb27d0ad974b850d7f3f6b5f0aae38be3792
Mike <pflueger.mike@gmail.com>
2016-06-01 10:16:29 -0400

    Play and Sound work

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class Paddle : MonoBehaviour {
 
     public float paddleSpeed = 1f;
-
+    public AudioClip ping;
     private Vector3 playerPos = new Vector3 (0, -9.5f, 0);
-	
-	// Update is called once per frame
 
-	void Update () {
+    // Update is called once per frame
+    void OnCollisionEnter(Collision other)
+    { AudioSource.PlayClipAtPoint(ping, playerPos);
+    }
+
+
+    void Update () {
         float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
         playerPos = new Vector3(Mathf.Clamp(xPos, -13f, 13f), -9.5f, 0f);
         transform.position = playerPos;
--- Assets/Scripts/brickBehavior.cs
+++ Assets/Scripts/brickBehavior.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class brickBehavior : MonoBehaviour {
     public int score;
-
+    public AudioClip brickbust;
     public GameObject brickParticle;
 
     void OnCollisionEnter(Collision other)
     {
         Instantiate(brickParticle, transform.position, Quaternion.identity);
         GM.instance.DestroyBrick(score);
+        AudioSource.PlayClipAtPoint(brickbust, gameObject.transform.position);
         Destroy(gameObject);
     }
 }
